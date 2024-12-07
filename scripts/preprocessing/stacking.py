f72f98b22b6e7bd3a5375e72dfe8882389619da9
redsonlopez <redsonlopez@gmail.com>
2024-12-07 13:31:53 -0300

    add columns to scripts/preprocessing/stacking.py

---
+++ scripts/preprocessing/stacking.py
@@ -0,0 +1,20 @@
+columns= [
+        "MATRICULA-CLIENTE-IMOVEL",
+        "VALOR-TOTal-FATura",
+        "VOLume-FATurado-Agua",
+        "VOLume-FATurado-Esgoto",
+        "VOLume-MEDido-A-agua",
+        "VOLume-MEDido-Esgoto",
+        "NOME-CLIENTE",
+        "HIDROMETRO",
+        "DATA-VENCIMENTO",
+        "TIPO-LOGRADOURO",
+        "NOME-LOGRADOURO",
+        "NUM-IMOVEL",
+        "COMPLemento-ENDERECO",
+        "INF-COMPLemento-ENDERECO",
+        "NOME-BAIRRO",
+        "NONE"
+]
+
+
