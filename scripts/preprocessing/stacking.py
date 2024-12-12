82940d357eaf14ad3e7cc217095021729516d705
redsonlopez <redsonlopez@gmail.com>
2024-12-12 15:58:27 -0300

    update scripts/preprocessing/stacking.py

--- scripts/preprocessing/stacking.py
+++ scripts/preprocessing/stacking.py
@@ -1,20 +1,31 @@
+import pandas as pd
+
+files= "202201_PBH.txt  202206_PBH.txt  202211_PBH.txt  202304_PBH.txt  202309_PBH.txt  202402_PBH.txt  202407_PBH.txt  202202_PBH.txt  202207_PBH.txt  202212_PBH.txt  202305_PBH.txt  202310_PBH.txt  202403_PBH.txt  202408_PBH.txt  202203_PBH.txt  202208_PBH.txt  202301_PBH.txt  202306_PBH.txt  202311_PBH.txt  202404_PBH.txt  202409_PBH.txt  202204_PBH.txt  202209_PBH.txt  202302_PBH.txt  202307_PBH.txt  202312_PBH.txt  202405_PBH.txt  202410_PBH.txt  202205_PBH.txt  202210_PBH.txt  202303_PBH.txt  202308_PBH.txt  202401_PBH.txt  202406_PBH.txt  202411_PBH.txt".split()
+
+file_list= []
+for file in files:
+	df= pd.read_csv(f"~/Projects/water_prediction/data/raw/{file}")
+	file_list.append(df)
+
+print(file_list)
+
 columns= [
-        "MATRICULA-CLIENTE-IMOVEL",
-        "VALOR-TOTal-FATura",
-        "VOLume-FATurado-Agua",
-        "VOLume-FATurado-Esgoto",
-        "VOLume-MEDido-A-agua",
-        "VOLume-MEDido-Esgoto",
-        "NOME-CLIENTE",
+        "MATRICULA",
+        "VALOR",
+        "VOL_FATURA_AGUA",
+        "VOL_FATURA_ESGOTO",
+        "VOL_MEDIDO_AGUA",
+        "VOL_MEDIDO_ESGOTO",
+        "CLIENTE",
         "HIDROMETRO",
-        "DATA-VENCIMENTO",
-        "TIPO-LOGRADOURO",
-        "NOME-LOGRADOURO",
-        "NUM-IMOVEL",
-        "COMPLemento-ENDERECO",
-        "INF-COMPLemento-ENDERECO",
-        "NOME-BAIRRO",
-        "NONE"
+        "DATA_VENCIMENTO",
+        "TIPO_LOGRADOURO",
+        "NOME_LOGRADOURO",
+        "NUM_IMOVEL",
+        "COMPLEMENTO_",
+        "INF_COMPLEMENTO",
+        "BAIRRO",
+        "residuo"
 ]
 
 
